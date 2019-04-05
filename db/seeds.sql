90d89075a06ca9166eaad9bf613251364ee23931
degalo93 <denisgalo10@gmail.com>
2019-04-04 18:05:35 -0700

    added orm and connect

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blt", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("All meat", true);
