4a8a44026ee80ae9502860223b833610958a2dd5
degalo93 <denisgalo10@gmail.com>
2019-04-05 20:05:26 -0700

    added code to model and controller

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,34 +6,36 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get('/', function(req, res) {
-    res.redirect('/index');
-});
 
 
-router.get('/index', function(req, res) {
+router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = { burgers: data };
         console.log(hbsObject);
-        res.render('index', hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
 
-router.post('/burgers/insertOne', function(req, res) {
-    burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
-        res.redirect('/index');
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function(data) {
+        res.redirect("index");
     });
 });
 
 
-router.put('/burgers/updateOne/:id', function(req, res) {
-    var condition = 'id = ' + req.params.id;
-    console.log('condition', condition);
+router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
 
     burger.updateOne({ devoured: req.body.devoured }, condition, function() {
-        res.redirect('/index');
+        res.redirect("index");
     });
 });
+
 
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Blt", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("All meat", true);
